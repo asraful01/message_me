f088de1338023bfddb71fac8792f8712402709e9
ashraful <ashsaas.islam@gmail.com>
2020-03-31 23:15:42 -0400

    generate chatroom channel, overflow, scrollbottom

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,11 @@
 //= require turbolinks
 //= require semantic-ui
 //= require_tree .
-
+scroll_bottom =function(){
+  if ($('#messages').length > 0){
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
 $(document).on('turbolinks:load', function()
 {
   $('.ui.dropdown')
@@ -26,5 +30,6 @@ $('.message .close').on('click', function() {
     $(this)
       .closest('.message').transition('fade');
     });
+    scroll_bottom();
 
 })
